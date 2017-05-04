5eb6268fb4281cadaf2b03638b3796639c70a49c
Colin Nickels <c.ni@me.com>
2017-05-04 14:56:02 -0400

    updated app url

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://queue-nickels.cloudapps.unc.edu/'),
 
     /*
     |--------------------------------------------------------------------------
